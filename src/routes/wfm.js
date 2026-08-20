36344c3529b93809bdac26b30b79ef5a958f14f2
dwightanthonybumaat-code <312327144+dwightanthonybumaat-code@users.noreply.github.com>
2026-08-20 13:37:09 +0800

    fix history logs in the wfm and added user login and logout i the history logs

--- src/routes/wfm.js
+++ src/routes/wfm.js
@@ -3,6 +3,7 @@ import express from "express";
 
 import {
   addHistoryLog,
+  clearHistoryLogs,
   getHistoryLogs,
 } from "../controllers/wfmHistoryLogController.js";
 import { getWfmCallsKpi } from "../controllers/wfmKpiController.js";
@@ -10,9 +11,6 @@ import authMiddleware from "../middleware/authMiddleware.js";
 import { requireRole } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
-
-router.get("/history-logs", getHistoryLogs);
-router.post("/history-logs", addHistoryLog);
 
 router.get("/imported-files", async (req, res) => {
   return res.json({
@@ -25,6 +23,24 @@ const requireWfm = [
   authMiddleware,
   requireRole([9]),
 ];
+
+router.get(
+  "/history-logs",
+  ...requireWfm,
+  getHistoryLogs,
+);
+
+router.post(
+  "/history-logs",
+  ...requireWfm,
+  addHistoryLog,
+);
+
+router.delete(
+  "/history-logs",
+  ...requireWfm,
+  clearHistoryLogs,
+);
 
 router.get(
   "/kpis/calls",
